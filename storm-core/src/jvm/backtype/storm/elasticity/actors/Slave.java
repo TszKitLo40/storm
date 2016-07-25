bf7bd238e30b27025f997f7f06e48168793be7eb
wangli1426 <wangli1426@gmail.com>
2016-07-25 16:05:55 +0800

    Slave ip can be mannually configured, when the ip automatically obtained from the system is not proper.

--- storm-core/src/jvm/backtype/storm/elasticity/actors/Slave.java
+++ storm-core/src/jvm/backtype/storm/elasticity/actors/Slave.java
@@ -9,6 +9,7 @@ import backtype.storm.elasticity.ElasticTaskHolder;
 import backtype.storm.elasticity.message.actormessage.*;
 import backtype.storm.elasticity.resource.ResourceMonitor;
 import backtype.storm.elasticity.routing.RoutingTable;
+import backtype.storm.elasticity.utils.ConfigReader;
 import backtype.storm.elasticity.utils.Histograms;
 import backtype.storm.elasticity.utils.timer.SmartTimer;
 import backtype.storm.generated.HostNotExistException;
@@ -410,16 +411,23 @@ public class Slave extends UntypedActor {
     }
 
     static public Slave createActor(String name, String port) {
+        backtype.storm.Config stormConfig;
         try{
+
+            String hostname = ConfigReader.readString("elasticity.slave.ip");
+            if(hostname == null)
+                hostname = InetAddress.getLocalHost().getHostAddress();
+
+            
             final Config config = ConfigFactory.parseString("akka.remote.netty.tcp.port=0")
-                    .withFallback(ConfigFactory.parseString("akka.remote.netty.tcp.hostname=" + InetAddress.getLocalHost().getHostAddress()))
+                    .withFallback(ConfigFactory.parseString("akka.remote.netty.tcp.hostname=" + hostname))
                     .withFallback(ConfigFactory.parseString("akka.cluster.roles = [slave]"))
                     .withFallback(ConfigFactory.parseString("akka.remote.netty.tcp.maximum-frame-size = 134217728"))
                     .withFallback(ConfigFactory.load());
             ActorSystem system = ActorSystem.create("ClusterSystem", config);
             system.actorOf(Props.create(Slave.class, name, port), "slave");
 
-            System.out.println("Slave actor is created!");
+            System.out.println("Slave actor is created on " + hostname);
 //            Slave slave = Slave.getInstance();
 
 
@@ -434,15 +442,22 @@ public class Slave extends UntypedActor {
 
     static public Slave createActor(String name, String port, boolean supervisorActor) {
         try{
+
+            String hostname = ConfigReader.readString("elasticity.slave.ip");
+            if(hostname == null)
+                hostname = InetAddress.getLocalHost().getHostAddress();
+
+            System.out.println("Host name = " + hostname);
+
             final Config config = ConfigFactory.parseString("akka.remote.netty.tcp.port=0")
-                    .withFallback(ConfigFactory.parseString("akka.remote.netty.tcp.hostname=" + InetAddress.getLocalHost().getHostAddress()))
+                    .withFallback(ConfigFactory.parseString("akka.remote.netty.tcp.hostname=" + hostname))
                     .withFallback(ConfigFactory.parseString("akka.cluster.roles = [slave]"))
                     .withFallback(ConfigFactory.parseString("akka.remote.netty.tcp.maximum-frame-size = 134217728"))
                     .withFallback(ConfigFactory.load());
             ActorSystem system = ActorSystem.create("ClusterSystem", config);
             system.actorOf(Props.create(Slave.class, name, port, supervisorActor), "slave");
 
-            System.out.println("Slave actor is created!");
+            System.out.println("Slave actor is created on " + hostname);
 //            Slave slave = Slave.getInstance();
 
 
--- storm-core/src/jvm/backtype/storm/elasticity/config/Config.java
+++ storm-core/src/jvm/backtype/storm/elasticity/config/Config.java
@@ -47,6 +47,6 @@ public class Config {
 
     public static double taskLevelLoadBalancingThreshold = 0.2;
 
-    public static String masterIp = "192.168.0.183";
+    public static String masterIp = "10.21.25.113";
 
 }
